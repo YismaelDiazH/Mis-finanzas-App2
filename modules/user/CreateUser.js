49afbf6476841c4c506f87bf29a5706e0be9ee16
YismaelDiazH <20213tn007@utez.edu.mx>
2023-02-23 03:30:37 -0600

    Crear usuario

--- modules/user/CreateUser.js
+++ modules/user/CreateUser.js
@@ -1,148 +1,177 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { isEmpty, size } from "lodash";
-import { Image, Input, Button } from "@rneui/base";
-import Loading from "../../kernel/components/Loading";
-import { Icon } from "react-native-elements";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { Input, Button, Image, Icon } from "@rneui/base";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { validateEmail } from "../../kernel/validation";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-export default function () {
-  const payload = {
-    email: "",
-    password: "",
-    repeatPassword: "",
-  };
-  const [show, setShow] = useState(false);
-  const [error, setError] = useState(payload);
-  const [data, setData] = useState(payload);
-  const [showPassword, setShowPassword] = useState(true);
-  const [showRepeatPassword, setShowRepeatPassword] = useState(true);
-  const changePayload = (e, type) => {
-    setData({ ...data, [type]: e.nativeEvent.text });
-  };
-  const createUser = () => {
-    if (!(isEmpty(data.email) || isEmpty(data.password))) {
-      if (validateEmail(data.email)) {
-        if (size(data.password) >= 6) {
-          if (data.password === data.repeatPassword) {
-            console.log("Listo calisto");
-            setError(payload);
-          } else {
-            setError({
-              email: "",
-              password: "La contraseña debe coincidir",
-              repeatPassword: "La contraseña debe coincidir",
-            });
-          }
-        } else {
-          setError({
-            email: "",
-            password: "La contraseña debe tener al menos 6 caracteres",
-          });
-        }
-      } else {
-        setError({
-          email: "Debe ser un correo electrónico",
-          password: "",
-          repeatPassword: "",
-        });
-      }
-    } else {
-      setError({
-        email: "Campo obligatorio",
-        repeatPassword: "Campo obligatorio",
-      });
-    }
-  };
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
+import React, { useState } from "react";
+import { isEmpty, size } from "lodash";
+import Loading from "../../kernel/components/Loading";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+export default function CreateUser(props) {
+  const { navigation } = props
+  const payLoad = {
+      email: '',
+      password: '',
+      repeatPassword: ''
+  }
+  const auth = getAuth()
+  const [show, setShow] = useState(false)
+  const [error, setError] = useState(payLoad)
+  const [data, setData] = useState(payLoad)
+  const [showPassword, setShowPassword] = useState(true)
+  const [showRepeatPassword, setShowRepeatPassword] = useState(true)
+  const changePayLoad = (e, type) => {
+      setData({ ...data, [type]: e.nativeEvent.text })
+  }
+  const createUser = () => {
+      console.log('CreateUser 24 -> data', data);
+      if (!(isEmpty(data.email || isEmpty(data.password)))) {
+          if (validateEmail(data.email)) {
+              if ((size(data.password)) >= 6) {
+                  if (data.password == data.repeatPassword) {
+                      setShow(true)
+                      setError(payLoad)
+                      console.log('Listo para el registro');
+                      createUserWithEmailAndPassword(auth, data.email, data.password)
+                          .then(async (userCredential) => {
+                              const user = userCredential.user;
+                              try {
+                                  await AsyncStorage.setItem('@session', JSON.stringify(user))
+                              } catch (e) {
+                                  console.error("Error -> createUser Storage", e);
+                              }
+                              console.log("Created User", user);
+                              setShow(false)
+                              navigation.navigate("profileStack")
+                          })
+                          .catch((error) => {
+                              setError({ email: '', password: 'No se pudo crear el usuario' })
+                              setShow(false)
+                              const errorCode = error.code;
+                              const errorMessage = error.message;
+                          });
+                  } else {
+                      setError({
+                          email: '',
+                          password: 'Debe coincidir con repetir contraseña',
+                          repeatPassword: 'Debe coincidir con contraseña'
+                      })
+                  }
+              } else {
+                  setError({
+                      email: '',
+                      password: 'Logitud de por lo menos 6 carácteres',
+                      repeatPassword: 'Logitud de por lo menos 6 carácteres'
+                  })
+              }
+          } else {
+              setError({
+                  email: 'Debe ser un correo electrónico válido',
+                  password: '',
+                  repeatPassword: ''
+              })
+          }
+      } else {
+          setError({
+              email: 'Campo obligatorio',
+              password: 'Campo obligatorio',
+              repeatPassword: 'Campo obligatorio'
+          })
+      }
+  }
   return (
-    <KeyboardAwareScrollView>
-      <Image
-        source={"../../assets/presupuesto.png"}
-        resizeMode="contain"
-        style={styles.logo}
-      />
-      <View style={styles.viewForm}>
-        <View style={styles.container}>
-          <Input
-            placeholder="Correo electrónico"
-            keyboardtype="Email-address"
-            rightIcon={
-              <Icon type="material-community" name="email" size={22} />
-            }
-            containerStyle={styles.input}
-            onChange={(e) => changePayload(e, "email")}
-            errorMessage={error.email}
-            autoCapitalize="none"
+      <KeyboardAwareScrollView>
+          <Image
+              source={require('../../assets/presupuesto.png')}
+              resizeMode='contain'
+              style={styles.logo}
           />
-          <Input
-            placeholder="Contraseña"
-            containerStyle={styles.input}
-            rightIcon={
-              <Icon
-                type="material-community"
-                name={showPassword ? "eye-off-outline" : "eye-outline"}
-                size={22}
-                onPress={() => setShowPassword(!showPassword)}
-              />
-            }
-            secureTextEntry={showPassword}
-            onChange={(e) => changePayload(e, "password")}
-            errorMessage={error.password}
-          />
-          <Input
-            placeholder="Contraseña"
-            containerStyle={styles.input}
-            rightIcon={
-              <Icon
-                type="material-community"
-                name={showRepeatPassword ? "eye-off-outline" : "eye-outline"}
-                size={22}
-                onPress={() => setShowRepeatPassword(!showRepeatPassword)}
-              />
-            }
-            secureTextEntry={showRepeatPassword}
-            onChange={(e) => changePayload(e, "repeatPassword")}
-            errorMessage={error.repeatPassword}
-          />
-          <Button
-            title="Crear cuenta"
-            containerStyle={styles.btnContainer}
-            buttonStyle={styles.btn}
-            onPress={createUser}
-          />
-        </View>
-      </View>
-      <Loading show={show} text="Registrar usuario" />
-    </KeyboardAwareScrollView>
-  );
+          <View style={styles.viewForm}>
+              <View style={styles.container}>
+                  <Input
+                      placeholder='Correo Electrónico'
+                      keyboardType='email-address'
+                      rightIcon={
+                          <Icon type='material-community' name='email' size={22} />
+                      }
+                      containerStyle={styles.input}
+                      onChange={(e) => changePayLoad(e, 'email')}
+                      errorMessage={error.email}
+                      autoCapitalize='none'
+                  />
+                  <Input
+                      placeholder='Contraseña'
+                      containerStyle={styles.input}
+                      rightIcon={
+                          <Icon
+                              type='material-community'
+                              name={showPassword ? 'eye-off-outline' : 'eye-outline'}
+                              onPress={() => setShowPassword(!showPassword)}
+                              size={22}
+                          />
+                      }
+                      secureTextEntry={showPassword}
+                      onChange={(e) => changePayLoad(e, 'password')}
+                      errorMessage={error.password}
+                  />
+                  <Input
+                      placeholder='Repetir contraseña'
+                      containerStyle={styles.input}
+                      rightIcon={
+                          <Icon
+                              type='material-community'
+                              name={showRepeatPassword ? 'eye-off-outline' : 'eye-outline'}
+                              onPress={() => setShowRepeatPassword(!showRepeatPassword)}
+                              size={22}
+                          />
+                      }
+                      secureTextEntry={showRepeatPassword}
+                      onChange={(e) => changePayLoad(e, 'repeatPassword')}
+                      errorMessage={error.repeatPassword}
+                  />
+                  <Button
+                      title='Crear cuenta'
+                      containerStyle={styles.btnContainer}
+                      buttonStyle={styles.btn}
+                      onPress={createUser}
+                  />
+              </View>
+          </View>
+          <Loading show={show} text='Registrando' />
+      </KeyboardAwareScrollView>
+  )
 }
 
 const styles = StyleSheet.create({
   logo: {
-    width: "100%",
-    height: 150,
-    marginTop: 20,
+      width: '100%',
+      height: 159,
+      marginTop: 20
   },
   viewForm: {
-    marginHorizontal: 20,
+      marginHorizontal: 20
   },
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 20,
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop: 20
   },
   input: {
-    width: "100%",
-    marginVertical: 10,
+      width: '100%',
+      marginVertical: 10
   },
   btnContainer: {
-    marginBottom: 20,
-    width: "95%",
+      marginBottom: 20,
+      width: '65%'
   },
   btn: {
-    color: "#28a745",
+      backgroundColor: '#0073db'
   },
-});
+})
